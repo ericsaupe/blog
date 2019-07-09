0dfb27701cae28eb46c821a291f6523e120809a6
Eric Saupe <ericsaupe@gmail.com>
2019-07-08 22:32:50 -0700

    Added Capybara support and Post system tests

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -3,10 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Home', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
   it 'displays hello, world' do
     visit '/'
     expect(page).to have_text('Hello, World!')
