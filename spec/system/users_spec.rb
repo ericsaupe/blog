70ddbd8793ca44a8e2698c4c52262dacc91e1d00
Eric Saupe <ericsaupe@gmail.com>
2019-07-29 22:20:01 -0700

    Added user sessions specs
    Testing that users can log in successfully, are presented with
    a non-specific error message when failing to login, and can
    log out properly once logged in.

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -55,5 +55,57 @@ RSpec.describe 'Users', type: :system do
         expect(page).to have_css('.notification.is-success')
       end
     end
+
+    describe 'session' do
+      let(:user) { create(:user) }
+
+      it 'signs in successfully' do
+        visit('/')
+        click_on('Log in')
+        fill_in('Email', with: user.email)
+        fill_in('Password', with: 'test1234')
+        within('form') do
+          click_on('Log in')
+        end
+        expect(page).to have_text('Signed in successfully.')
+        expect(page).to have_css('.notification.is-success')
+      end
+
+      it 'requires all fields' do
+        visit('/')
+        click_on('Log in')
+        within('form') do
+          click_on('Log in')
+        end
+        expect(page).not_to have_text('Signed in successfully.')
+        expect(page).to have_text('Invalid Email or password.')
+
+        fill_in('Email', with: user.email)
+        within('form') do
+          click_on('Log in')
+        end
+        expect(page).not_to have_text('Signed in successfully.')
+        expect(page).to have_text('Invalid Email or password.')
+
+        fill_in('Password', with: 'test1234')
+        within('form') do
+          click_on('Log in')
+        end
+        expect(page).to have_text('Signed in successfully.')
+        expect(page).not_to have_text('Invalid Email or password.')
+      end
+
+      it 'signs out successfully' do
+        login_as(user, scope: :user)
+
+        visit('/')
+        expect(page).to have_link('Sign out')
+        click_on('Sign out')
+        expect(page).to have_text('Signed out successfully.')
+        expect(page).not_to have_link('Sign out')
+        expect(page).to have_link('Sign up')
+        expect(page).to have_link('Log in')
+      end
+    end
   end
 end
