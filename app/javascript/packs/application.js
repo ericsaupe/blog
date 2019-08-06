5dba34f64b74e471a70ce783dea9c0e41aa2b111
Eric Saupe <ericsaupe@gmail.com>
2019-08-05 21:59:32 -0700

    Installed stimulus and added flash_controller
    Our stimlus controller called flash_controller handles the close
    button on notifications that are added via Rails. The notifications
    will close automatically or allow the user to close them with the
    delete button.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,12 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 require("trix")
 require("@rails/actiontext")
